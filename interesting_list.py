555fa6c1afcfdc035431adf18c3d8271b036ccb3
yuchienchen <yuchienc@yahoo.com>
2024-07-11 15:45:52 -0500

    lift comprehension

--- interesting_list.py
+++ interesting_list.py
@@ -1,19 +1,21 @@
-# num_list = [-2, 33, 14, 6, -13, 9, 2]
+num_list = [-2, 33, 14, 6, -13, 9, 2]
 
 def make_interesting_list(num_list):
     
-    new_list = []
-    for elem in num_list:
-        if elem < 0:
-           del elem
-        if elem >= 0 and elem % 2 == 0 :
-           elem = elem
-           new_list.append(elem)
-        else:
-           elem = elem + 10
-           new_list.append(elem)
+    num_list = [elem for elem in num_list if elem >= 0]
+    # new_list = []
+    # return num_list
 
-    return new_list
+    # print(num_list)
+
+    #     if elem >= 0 and elem % 2 == 0 :
+    #        elem = elem
+    #        new_list.append(elem)
+    #     else:
+    #        elem = elem + 10
+    #        new_list.append(elem)
+
+    # return new_list
   
 
-print(make_interesting_list([-2, 33, 14, 6, -13, 9, 2]))
+print(make_interesting_list(num_list))

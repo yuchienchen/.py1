0eb3199be0272a2e0a6222d78a8bba8c6c3810ff
yuchienchen <yuchienc@yahoo.com>
2024-07-11 16:11:27 -0500

    Final

--- interesting_list.py
+++ interesting_list.py
@@ -2,11 +2,11 @@ num_list = [-2, 33, 14, 6, -13, 9, 2]
 
 # def make_interesting_list(num_list):
     
-num_list = [elem for elem in num_list if elem >= 0 and elem % 2 == 0]
-print(num_list)
+# num_list = [elem for elem in num_list if elem >= 0 and elem % 2 == 0]
+# print(num_list)
 
 # no need for the assignment: elem = elem + 10
-num_list = [elem + 10 for elem in num_list if elem >= 0 and elem % 2 == 1]
+num_list = [elem + 10 if elem % 2 == 1 else elem for elem in num_list if elem >= 0]
 print(num_list)
 
 

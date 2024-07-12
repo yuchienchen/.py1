a34199be25a87a31e40cd434a1073a6171628579
yuchienchen <yuchienc@yahoo.com>
2024-07-12 17:49:05 -0500

    grid could have any number of lists with any number of item types

--- groceries.py
+++ groceries.py
@@ -1,9 +1,17 @@
 # create individual list
-list = []
-for i in range(4):
-    list.append(input(""))
+def get_grocery_list():
 
-print(list)
+    grocery_list = []
+    while True:
+        user_input = input("")
+
+        # If the user enters a blank line, break out of the loop and stop asking for input
+        if user_input == "":
+            break
+    
+        grocery_list.append(user_input)
+
+    return grocery_list
 
 def get_groceries(index, grid):
     pass

020af0170606f353f449e927229d54a2fbe9bf32
yuchienchen <yuchienc@yahoo.com>
2024-07-15 17:38:17 -0500

    print average temps

--- average_temps.py
+++ average_temps.py
@@ -6,12 +6,18 @@ def main():
     annual_temps(nested)
 
 def annual_temps(nested):
-    # ave_temps = []
+    ave_temps = []
     rows = len(nested)
     cols = len(nested[0])
     for i in range(rows):
         print(nested[i])
         print(sum(nested[i]))
+        average_temp = sum(nested[i]) / len(nested[i])
+        print(average_temp)
+    #     ave_temps.append(average_temp)
+    
+    # return ave_temps
+
         # for j in range(cols):
         #     print(nested[i][j])
         #     average_temp =  / len(nested[i])

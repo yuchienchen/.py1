f67bc7f37ef2fde352b779b1b9f81c73cc9827d0
yuchienchen <yuchienc@yahoo.com>
2024-07-16 17:54:49 -0500

    swap?

--- fireworks.py
+++ fireworks.py
@@ -3,16 +3,27 @@ def main():
         [None, None, None],
         [None, 'f' , None],
         [None, None, None]]
-    explode_firewords(grid)
+    explode_fireworks(grid)
 
-def explode_firewords(grid):
+def explode_fireworks(grid):
     rows = len(grid)
-    print(rows)
+    # print(rows)
     cols = len(grid[0])
-    print(cols)
-    # for i in range(rows):
-        # for j in range(cols):
-        #     print(grid[i][j])
+    # print(cols)
+    for y in range(rows):
+        for x in range(cols):
+            # print(grid[y][x])
+            if grid[y][x] == 'f':
+                grid[y - 1][x] == 's'
+                grid[y + 1][x] == 's'
+                grid[y][x - 1] == 's'
+                grid[y][x + 1] == 's'
+                grid[y][x] == None
+                
+def swap(grid, row1, col1, row2, col2):
+    temp = grid[row1][col1]
+    grid[row1][col1] = grid[row2][col2]
+    grid[row2][col2] = temp
 
 
 if __name__ == '__main__':

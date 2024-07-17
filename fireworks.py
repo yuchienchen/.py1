b8ee4ac40886ac1fccd3ddf833206a24086c65ed
yuchienchen <yuchienc@yahoo.com>
2024-07-17 17:16:01 -0500

    list comprehension initial

--- fireworks.py
+++ fireworks.py
@@ -13,17 +13,17 @@ def explode_fireworks(grid):
     for y in range(rows):
         for x in range(cols):
             # print(grid[y][x])
-            if grid[y][x] == 'f':
-                grid[y - 1][x] == 's'
-                grid[y + 1][x] == 's'
-                grid[y][x - 1] == 's'
-                grid[y][x + 1] == 's'
-                grid[y][x] == None
+            grid = [grid[y][x] == None, grid[y - 1][x] == 's', grid[y + 1][x] == 's', grid[y][x - 1] == 's', grid[y][x + 1] == 's' if grid[y][x] == 'f' else None]
+    print(grid)
+
+
+            # if grid[y][x] == 'f':
+            #     grid[y - 1][x] == 's'
+            #     grid[y + 1][x] == 's'
+            #     grid[y][x - 1] == 's'
+            #     grid[y][x + 1] == 's'
+            #     grid[y][x] == None
                 
-def swap(grid, row1, col1, row2, col2):
-    temp = grid[row1][col1]
-    grid[row1][col1] = grid[row2][col2]
-    grid[row2][col2] = temp
 
 
 if __name__ == '__main__':
